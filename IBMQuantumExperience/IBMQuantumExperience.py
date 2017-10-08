a3dde42ca7b64fe2aaf924c9a4336d69034f6a58
juan-cb <juancb@usal.es>
2017-10-08 13:59:13 +0200

    Updating comments

--- IBMQuantumExperience/IBMQuantumExperience.py
+++ IBMQuantumExperience/IBMQuantumExperience.py
@@ -1329,7 +1329,7 @@ class IBMQuantumExperience(object):
     def get_backend_by_name(self, name,
                             access_token=None, user_id=None):
         """
-        Create a backend by admin
+        Get a backend by name
         """
         if access_token:
             self.req.credential.set_token(access_token)
@@ -1350,7 +1350,7 @@ class IBMQuantumExperience(object):
 
     def get_backends(self, access_token=None, user_id=None):
         """
-        Create a backend by admin
+        Get all backends by admin
         """
         if access_token:
             self.req.credential.set_token(access_token)
